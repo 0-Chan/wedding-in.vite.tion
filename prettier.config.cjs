6a20ea0ce2cc90e41ca3d4155b75ccbebc5f7181
0-Chan <kyc4421194@gmail.com>
2023-03-09 23:45:18 +0900

    Setup Prettier with plugins
    Plugins are TailwindCSS plugin and import plugin.

---
+++ prettier.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  pluginSearchDirs: false,
+  printWidth: 80,
+  tabWidth: 4,
+  trailingComma: 'all',
+  singleQuote: true,
+  semi: true,
+  importOrder: [
+      '^react/(.*)$',
+      '^[./]',
+  ],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+};
