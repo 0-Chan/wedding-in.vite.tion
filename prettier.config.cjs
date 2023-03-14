5d325fef6b0d7124bb9320c7e04d3542450d7fc9
0-Chan <kyc4421194@gmail.com>
2023-03-14 12:15:28 +0900

    Fix code style

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,14 +1,11 @@
 module.exports = {
   pluginSearchDirs: false,
   printWidth: 80,
-  tabWidth: 4,
+  tabWidth: 2,
   trailingComma: 'all',
   singleQuote: true,
   semi: true,
-  importOrder: [
-      '^react/(.*)$',
-      '^[./]',
-  ],
+  importOrder: ['^react/(.*)$', '^[./]'],
   importOrderSeparation: true,
   importOrderSortSpecifiers: true,
 };
