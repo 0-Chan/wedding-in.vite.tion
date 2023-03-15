d1d90ea3fb91f37dc7813c8b209215da9e236c88
0-Chan <kyc4421194@gmail.com>
2023-03-16 01:23:43 +0900

    Remove comment

--- src/component/GreetingConfetti.jsx
+++ src/component/GreetingConfetti.jsx
@@ -5,9 +5,6 @@ import { loadFull } from 'tsparticles';
 const GreetingConfetti = () => {
   const particlesInit = useCallback(async (engine) => {
     console.log(engine);
-    // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
-    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-    // starting from v2 you can add only the features you need reducing the bundle size
     await loadFull(engine);
   }, []);
 
@@ -104,7 +101,7 @@ const GreetingConfetti = () => {
             },
           },
           shape: {
-            type: ['character', 'character', 'character'],
+            type: ['character'],
             options: {
               character: [
                 {
